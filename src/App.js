83fabb701177ca89d95844c822b20664a05d7801
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-03-27 19:08:51 +0530

    improv: set height of react entrypoint div equal to screen size

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
