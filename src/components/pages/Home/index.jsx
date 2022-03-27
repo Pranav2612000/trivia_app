0043e6c39cf74cb0b43fc1bd2ae23b806d04f3ab
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-03-27 16:43:26 +0530

    feat: implement basic Home Route

---
+++ src/components/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome to Trivia Challenge</h1>
+      <div>
+        <p>You will be presented with 10 True or False Questions</p>
+        <p>Can you score 100%?</p>
+      </div>
+      <button>Begin</button>
+    </>
+  );
+};
+export default Home;
