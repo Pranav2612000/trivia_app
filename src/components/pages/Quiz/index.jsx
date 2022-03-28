624953dccfeed1f431c64bd41d6b94cbee1ce345
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-03-28 22:11:56 +0530

    feat: add loading state while questions are being fetched

--- src/components/pages/Quiz/index.jsx
+++ src/components/pages/Quiz/index.jsx
@@ -5,20 +5,32 @@ import Question from "../../Question";
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const data = await fetchQuestions();
       setQuestions(data);
+      setLoading(false);
     };
 
     fetchData();
   }, []);
   return (
     <>
+      {loading && (
+        <Typography
+          text="Loading..."
+          container="p"
+          variant="secondary"
+          size="content"
+          className="mt-20"
+        />
+      )}
       {questions.length > 0 && (
         <Question category={questions[0].category} question={questions[0].question} />
       )}
-      {questions.length === 0 && (
+      {!loading && questions.length === 0 && (
         <Typography
           text="Questions could not be fetched. Try reloading the page!"
           container="p"
