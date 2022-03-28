8d8a16540c3906b5c8bed7fe2caaec72320e7a77
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-03-28 22:09:11 +0530

    feat: modify custombutton to allow onClick events

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -3,9 +3,14 @@ import clsx from "clsx";
 
 const getClassFromVariantName = (variant) => `btn-${variant}`;
 
-const Button = ({ text, variant }) => {
+const Button = ({ text, variant, onClick }) => {
   return (
-    <button className={clsx("btn", "btn-primary", getClassFromVariantName(variant))}>{text}</button>
+    <button
+      className={clsx("btn", "btn-primary", getClassFromVariantName(variant))}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 };
 export default Button;
